a25ce35010378fe80a0a0b6c9e6653fc15822c9f
20CSESumanth <112487116+20CSE452-Sumanth@users.noreply.github.com>
2022-09-27 10:16:13 +0530

    Update Linear_search.c

--- Linear_search.c
+++ Linear_search.c
@@ -21,33 +21,7 @@ void main()
         }
     }
     if(flag==1)
-        printf("Element %d is Found at %d",key,i);
-    else
-    printf("The element is not found");
-}#include<stdio.h>
-void main()
-{
-    int n;
-    printf("Enter the size of the array\n");
-    scanf("%d",&n);
-    int a[n],flag=0,i,key;
-    printf("Enter the array elements\n");
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("\nEnter the key element to search\n");
-    scanf("%d",&key);
-    for(i=0;i<n;i++)
-    {
-        if(a[i]==key)
-        {
-            flag=1;
-            break;
-        }
-    }
-    if(flag==1)
-        printf("Element %d is Found at %d",key,i);
+        printf("Element %d is Found at %d",key,mid);
     else
     printf("The element is not found");
 }
