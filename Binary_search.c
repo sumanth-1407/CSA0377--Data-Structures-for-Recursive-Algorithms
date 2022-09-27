071ea9f67237d789042d40142410828d0479fd5a
20CSESumanth <112487116+20CSE452-Sumanth@users.noreply.github.com>
2022-09-27 09:35:40 +0530

    Add files via upload

---
+++ Binary_search.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    int a[n],flag=0,i,first=0,last,mid,key;
+    last=n-1;
+    printf("Enter the array elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the Element to search\n");
+    scanf("%d",&key);
+    while (first<last)
+    {
+        mid=(first+last)/2;
+        if(a[mid]==key)
+        {
+            flag=1;
+            break;
+        }
+        else if(a[mid]>key)
+        last=mid-1;
+        else
+        first=mid+1;
+    }
+    if(flag==1)
+    {
+        printf("Element %d is found at %d",key,i);
+    }
+    else
+    printf("The element is not found");
+}
---
+++ Linear_search.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    int a[n],flag=0,i,key;
+    printf("Enter the array elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\nEnter the key element to search\n");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+        printf("Element %d is Found at %d",key,i);
+    else
+    printf("The element is not found");
+}#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    int a[n],flag=0,i,key;
+    printf("Enter the array elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\nEnter the key element to search\n");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+        printf("Element %d is Found at %d",key,i);
+    else
+    printf("The element is not found");
+}
